b295e9b11d415e2d42697349e288d822172b807b
SeniorSSSS <sakariye.sed@outlook.com>
2022-05-31 22:13:43 -0400

    Add grid using css & javascript, add trailing effect for boxes

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-// selects thing in html, makes node, add id to node for styling, appends node to html
 const mainGridElem =document.querySelector('#maincontainer');
 
-for (var i=0; i < 256; i++) {
+
+
+
+for (var i = 0; i < 256; i++) {
     const divnode = document.createElement("div");
     divnode.id =  "griditems";
     mainGridElem.appendChild(divnode);
-    divnode.innerText = 'hi';
+    divnode.innerText = 'hi' + i;
 }
 
-divnode.addEventListener("mouseover", hover());
 
+divnode = document.querySelectorAll('#griditems');
+var divnodearray = Array.from(divnode);
 
-// add functions that looks into dom for the his and then if you hover does hover()
-
-
-
-
-
-function hover() {
-    divnode.id =griditemshovered;
+for (var i = 0; i < divnodearray.length; i++) {
+    divnodearray[i].addEventListener('mouseover', hover);;
+    divnodearray[i].addEventListener ('mouseout',hoverexit)
 }
+
+function hover(event) {
+    event.target.id= "griditemshover";
+}
+function hoverexit(event) {
+    event.target.id= "griditems";
+}
+
+
+const buttonElem = document.querySelector('button');
+
+console.log (buttonElem);
+
+
