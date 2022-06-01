d2920869a2f95d1ca126b8fd9b8a52bf1f6f210e
SeniorSSSS <sakariye.sed@outlook.com>
2022-06-01 08:18:49 -0400

    Add button to chose number of element in place

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const mainGridElem =document.querySelector('#maincontainer');
 
-
-
-
 for (var i = 0; i < 256; i++) {
     const divnode = document.createElement("div");
     divnode.id =  "griditems";
@@ -10,15 +7,12 @@ for (var i = 0; i < 256; i++) {
     divnode.innerText = 'hi' + i;
 }
 
-
 divnode = document.querySelectorAll('#griditems');
 var divnodearray = Array.from(divnode);
-
 for (var i = 0; i < divnodearray.length; i++) {
     divnodearray[i].addEventListener('mouseover', hover);;
     divnodearray[i].addEventListener ('mouseout',hoverexit)
 }
-
 function hover(event) {
     event.target.id= "griditemshover";
 }
@@ -28,7 +22,30 @@ function hoverexit(event) {
 
 
 const buttonElem = document.querySelector('button');
+buttonElem.addEventListener('click', gridgenerator); 
+function gridgenerator(event) {
+    mainGridElem.replaceChildren();
+    let j = gridnumber();
+    for (var i = 0; i < j; i++) {
+        const divnode = document.createElement("div");
+        divnode.id =  "griditems";
+        mainGridElem.appendChild(divnode);
+        divnode.innerText = 'hi' + i;
+    }
+}
+function gridnumber(yourchoice) {
+    yourchoice = prompt("Choice the number of squares man which is less than 100");
 
-console.log (buttonElem);
+    if (yourchoice > 100) {
+        yourchoice = prompt("A number less than 100 is required");
+    }
+    else if (yourchoice <= 100)
+        alert ('Your grid will now be made');
+        return yourchoice;
+    }
+   
+
+
+
 
 
